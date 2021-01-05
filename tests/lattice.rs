a2bcac133c5247974cb7200a5466d8205531d706
Jamey Sharp <jamey@minilop.net>
2021-01-05 13:24:46 -0800

    Test lattices over larger models
    For small numbers of variables (up to 4) this test is exhaustive. For
    5-9 variables it tests the models near both ends of the lattice but
    stops after testing a bounded number of models.

--- tests/lattice.rs
+++ tests/lattice.rs
@@ -1,34 +1,96 @@
 use reconstructability::Model;
 use std::collections::{HashMap, HashSet};
 
-#[test]
-fn reversible_lattice() {
-    let mut saturated = Model::new();
-    saturated.add_relation((0u8..5).into_iter().collect());
-
-    let mut current_layer = vec![(saturated, HashSet::new())];
-    let mut next_layer = HashMap::new();
-
-    while !current_layer.is_empty() {
-        for (model, parents) in current_layer.drain(..) {
-            let mut computed = HashSet::new();
-            for parent in model.more_complex() {
-                let unique = computed.insert(parent);
-                // more_complex must not produce duplicates
-                assert!(unique);
-            }
-            assert_eq!(computed, parents);
-
-            for next in model.less_complex() {
-                let unique = next_layer
-                    .entry(next)
-                    .or_insert_with(HashSet::new)
-                    .insert(model.clone());
-                // less_complex must not produce duplicates
-                assert!(unique);
-            }
+macro_rules! check_size {
+    ($($name:ident)*) => {
+        $(
+        #[test]
+        fn $name() {
+            check(stringify!($name).as_bytes().last().unwrap() - b'0');
         }
-
-        current_layer.extend(next_layer.drain());
+        )*
     }
+}
+
+check_size!{
+    lattice_over_2
+    lattice_over_3
+    lattice_over_4
+    lattice_over_5
+    lattice_over_6
+    lattice_over_7
+    lattice_over_8
+    lattice_over_9
+}
+
+fn check(variables: u8) {
+    let variables = (0..variables).into_iter().collect();
+    let independence = Model::independence(&variables);
+    let mut saturated = Model::new();
+    saturated.add_relation(variables);
+
+    let mut seen = 0;
+    let mut current_layer = vec![(saturated, HashSet::new())];
+    while !current_layer.is_empty() && seen < 1000 {
+        //println!("down from {} models", current_layer.len());
+        check_down(&mut current_layer);
+        seen += current_layer.len();
+    }
+
+    let mut seen = 0;
+    current_layer.clear();
+    current_layer.push((independence, HashSet::new()));
+    while !current_layer.is_empty() && seen < 1000 {
+        //println!("up from {} models", current_layer.len());
+        check_up(&mut current_layer);
+        seen += current_layer.len();
+    }
+}
+
+fn check_down(current_layer: &mut Vec<(Model<u8>, HashSet<Model<u8>>)>) {
+    let mut next_layer = HashMap::new();
+    for (model, progenitors) in current_layer.drain(..) {
+        let mut computed = HashSet::new();
+        for progenitor in model.more_complex() {
+            let unique = computed.insert(progenitor);
+            // more_complex must not produce duplicates
+            assert!(unique);
+        }
+        assert_eq!(computed, progenitors);
+
+        for next in model.less_complex() {
+            let unique = next_layer
+                .entry(next)
+                .or_insert_with(HashSet::new)
+                .insert(model.clone());
+            // less_complex must not produce duplicates
+            assert!(unique);
+        }
+    }
+
+    current_layer.extend(next_layer.drain());
+}
+
+fn check_up(current_layer: &mut Vec<(Model<u8>, HashSet<Model<u8>>)>) {
+    let mut next_layer = HashMap::new();
+    for (model, progenitors) in current_layer.drain(..) {
+        let mut computed = HashSet::new();
+        for progenitor in model.less_complex() {
+            let unique = computed.insert(progenitor);
+            // less_complex must not produce duplicates
+            assert!(unique);
+        }
+        assert_eq!(computed, progenitors);
+
+        for next in model.more_complex() {
+            let unique = next_layer
+                .entry(next)
+                .or_insert_with(HashSet::new)
+                .insert(model.clone());
+            // more_complex must not produce duplicates
+            assert!(unique);
+        }
+    }
+
+    current_layer.extend(next_layer.drain());
 }
